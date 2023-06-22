2a9103c705a0ed740a01aecdb6bc753496180eae
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-06-22 15:21:42 -0300

    ...

--- componentes/cabecalhoPerfil/index.js
+++ componentes/cabecalhoPerfil/index.js
@@ -2,15 +2,76 @@ import imgSetaEsquerda from '../../public/imagens/setaEsquerda.svg';
 import CabecalhoComAcoes from '../cabecalhoComAcoes';
 import Botao from '../botao';
 import Avatar from '../avatar';
+import { useEffect, useState } from 'react';
+import UsuarioService from '@/services/UsuarioService';
+import { useRouter } from 'next/router';
+
+const usuarioService = new UsuarioService();
 
 export default function CabecalhoPerfil({
-    usuario
+    usuario,
+    estaNoPerfilPessoal
 }) {
+    const [estaSeguindoOUsuario, setEstaSeguindoOUsuario] = useState(false);
+    const [quantidadeSeguidores, setQuantidadeSeguidores] = useState(0);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!usuario) {
+            return;
+        }
+
+        setEstaSeguindoOUsuario(usuario.segueEsseUsuario);
+        setQuantidadeSeguidores(usuario.seguidores);
+    }, [usuario]);
+
+    const obterTextoBotaoSeguir = () => {
+        if (estaNoPerfilPessoal) {
+            return 'Editar Perfil';
+        }
+        if (estaSeguindoOUsuario) {
+            return 'Deixar de seguir';
+        }
+
+        return 'Seguir';
+    }
+
+    const obterCorDoBotaoSeguir = () => {
+        if (estaSeguindoOUsuario) {
+            return 'invertido';
+        }
+
+        return 'primaria';
+    }
+
+    const manipularCliqueBotaoPrincipal = async () => {
+        if (estaNoPerfilPessoal) {
+            return router.push('/perfil/editar');
+        }
+
+        try {
+            await usuarioService.altenarSeguir(usuario._id);
+            setQuantidadeSeguidores(
+                estaSeguindoOUsuario
+                    ? (quantidadeSeguidores - 1)
+                    : (quantidadeSeguidores + 1) 
+            );
+            setEstaSeguindoOUsuario(!estaSeguindoOUsuario);
+        } catch (error) {
+            alert(`Erro ao seguir/deixar de seguir!`);
+        }
+    }
+
+    const aoClicarSetaEsquerda = () => {
+        router.back();
+    }
+
     return (
         <div className='cabecalhoPerfil largura30pctDesktop'>
             <CabecalhoComAcoes
                 iconeEsquerda={imgSetaEsquerda}
-                titulo={usuario?.nome}
+                aoClicarAcaoEsquerda={aoClicarSetaEsquerda}
+                titulo={usuario.nome}
             />
 
             <hr className='bordaCabecalhoPerfil' />
@@ -20,24 +81,25 @@ export default function CabecalhoPerfil({
                 <div className='informacoesPerfil'>
                     <div className='statusContainer'>
                         <div className='status'>
-                            <strong>{usuario?.publicacoes}</strong>
+                            <strong>{usuario.publicacoes}</strong>
                             <span>Publicações</span>
                         </div>
 
                         <div className='status'>
-                            <strong>{usuario?.seguidores}</strong>
+                            <strong>{quantidadeSeguidores}</strong>
                             <span>Seguidores</span>
                         </div>
 
                         <div className='status'>
-                            <strong>{usuario?.seguindo}</strong>
+                            <strong>{usuario.seguindo}</strong>
                             <span>Seguindo</span>
                         </div>
                     </div>
 
                     <Botao
-                        texto={'Seguir'}
-                        cor='primaria'
+                        texto={obterTextoBotaoSeguir()}
+                        cor={obterCorDoBotaoSeguir()}
+                        manipularClique={manipularCliqueBotaoPrincipal}
                     />
                 </div>
             </div>
--- componentes/feed/index.js
+++ componentes/feed/index.js
@@ -4,18 +4,19 @@ import FeedService from "@/services/FeedService";
 
 const feedService = new FeedService();
 
-export default function Feed({ usuarioLogado, idUsuario }) {
+export default function Feed({ usuarioLogado, usuario }) {
   const [listaDePostagens, setListaDePostagens] = useState([]);
 
   useEffect(() => {
+    setListaDePostagens([]);
     const carregarPostagens = async () => {
-      const { data } = await feedService.carregarPostagens(idUsuario);
+      const { data } = await feedService.carregarPostagens(usuario?._id);
       const postagensFormatadas = data.map((postagem) => ({
         id: postagem._id,
         usuario: {
           id: postagem.idUsuario,
-          nome: postagem.usuario.nome,
-          avatar: postagem.usuario.avatar
+          nome: usuario?.nome || postagem?.usuario?.nome,
+          avatar: usuario?.avatar || postagem?.usuario?.avatar
         },
         fotoDoPost: postagem.foto,
         descricao: postagem.descricao,
@@ -29,17 +30,22 @@ export default function Feed({ usuarioLogado, idUsuario }) {
     };
 
     carregarPostagens();
-  }, [usuarioLogado, idUsuario]);
+  }, [usuarioLogado, usuario]);
+
+  if (!listaDePostagens.length) {
+    return null;
+  }
 
   return (
     <div className="feedContainer largura30pctDesktop">
-      {listaDePostagens.map((dadosPostagem) => (
-        <Postagem
-          key={dadosPostagem.id}
-          {...dadosPostagem}
-          usuarioLogado={usuarioLogado}
-        />
-      ))}
+        {listaDePostagens.map((dadosPostagem) => (
+          <Postagem
+            key={dadosPostagem.id}
+            {...dadosPostagem}
+            usuarioLogado={usuarioLogado}
+          />
+        ))
+      }
     </div>
   );
 }
